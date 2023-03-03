d6912f1d3691819048845331754632cc037d3e14
vignesh <vignesh296169@gmail.com>
2023-03-03 14:36:15 +0530

    cmt

--- src/Projects/ProjectList.js
+++ src/Projects/ProjectList.js
@@ -4,19 +4,31 @@ import projects from "../projectfile/Project.json";
 import Projectitem from "./Projectitem";
 
 import { Container } from "@mui/system";
-
+import { motion } from "framer-motion";
 export default function ProjectList() {
+    const varian={
+        hidden:{
+            x:-100,
+            opacity:0
+        },visible:{
+            y:10,
+            x:0,
+            opacity:1,
+           
+
+        }
+    }
   return (
     <Container>
     <ul >
      
       {projects.projects.map((project) => {
         return (
-          
-              <Paper key={project.id} sx={{marginBottom:"3px"}}>
+               <motion.div key={project.id} variants={varian} initial="hidden" animate="visible" transition={{duration:".5"}}>
+              <Paper  sx={{marginBottom:"3px"}}>
                 <Projectitem value={project} />
               </Paper>
-           
+              </motion.div>
         );
       })}
       
